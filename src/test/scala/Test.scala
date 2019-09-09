7b76640371cd3e1617ce1a291a1244d5cadbe5b6
aophelie <oamolomakama.pv@gmail.com>
2019-09-09 21:55:30 +0200

    Version finale avec le rapport

--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -7,10 +7,17 @@ class Test extends FlatSpec with Matchers with GeneratorDrivenPropertyChecks {
   "convertToInt" should "convert a String to Int" in {
 
     val intString = "convertToInt"("1") {
-      Or.first(1)
+      Or.first("1")
     }
     "intString" should contain(1)
   }
 
+  "bimap" should "transform a String or a Int to another one" in {
+
+    /*val intBimap = "bimap"("1")(_ + 1, _.length) {
+      convertToInt("1").bimap(_ + 1, _.length)
+    }
+    "intBimap" should contain(2)*/
+  }
 
 }
